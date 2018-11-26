5bea4bb906b61772b77f2eb3b52c6205399eba3b
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-26 20:54:06 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,9 @@
 def my_each(array)
   iterator = 0
-
-  if block_given?
-    while iterator < array.length
-      yield(array[iterator])
-      iterator += 1
-    end
-    array
-  else
-    puts "give me a block please :)"
+  
+  while iterator < array.length
+    yield(array[iterator])
+    iterator += 1
   end
+  array
 end
